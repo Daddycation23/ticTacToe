7048ef5b496b892dfe075a90f4d0cd46d36197b4
2403453 <2403453@sit.singaporetech.edu.sg>
2024-11-19 02:50:49 +0800

    Update NBmodel.c

--- NBModel/NBmodel.c
+++ NBModel/NBmodel.c
@@ -5,7 +5,7 @@
 
 // Function to train Naive Bayes model
 void train_NBmodel(NaiveBayesModel *model, char boards[][NUM_POSITIONS + 1], int outcomes[], int size) {
-    int positive_count = 0, negative_count = 0;
+    int positive_count = 0, negative_count = 0;            // Count for the occurances of the two different outcomes "postive" and "negative" in the dataset
 
     // Initialize counts
     int x_counts[NUM_POSITIONS][NUM_OUTCOMES] = {0};        // Array for number of occurances of 'x' in every position for both 'postive' and 'negative' outcome
