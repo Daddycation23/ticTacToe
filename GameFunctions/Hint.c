9be7112dec244a2c3658d2d80f1edb1c234c92b9
Terenceisstudying <terencetanbingkun@gmail.com>
2024-11-27 17:09:16 +0800

    added terence declaration

--- GameFunctions/Hint.c
+++ GameFunctions/Hint.c
@@ -12,17 +12,17 @@ void clearHint() {
 
 // Get Hint
 void getHint() {
-    int bestScore = -1000;
+    int bestScore = -1000; // Initialize the best score to a very low value
     int bestRow = -1;
     int bestCol = -1;
     int depthLimit = 9; // Full depth
     for (int i = 0; i < GRID_SIZE; i++) {
-        for (int j = 0; j < GRID_SIZE; j++) {
-            if (grid[i][j] == EMPTY) {
-                grid[i][j] = PLAYER_O;
-                int score = Minimax(grid, false, 0, depthLimit, -1000, 1000);
-                grid[i][j] = EMPTY;
-
+        for (int j = 0; j < GRID_SIZE; j++) { // Iterate through each cell in the grid
+            if (grid[i][j] == EMPTY) { // If the cell is empty
+                grid[i][j] = PLAYER_O; // Set the cell to PLAYER_O
+                int score = Minimax(grid, false, 0, depthLimit, -1000, 1000); // Set the score
+                grid[i][j] = EMPTY; // Clear the cell
+                // Save best move
                 if (score > bestScore) {
                     bestScore = score;
                     bestRow = i;
